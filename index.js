938c2615a00f699f7c40f813ad030e9aef3781fd
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-21 23:57:50 +0530

    logs added

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.post("/webhook", (req, res) => {
  console.log(JSON.stringify(body_param, null, 2));
 
  if(body_param.object){
+    console.log("inside body_param");
     if(body_param.entry && 
         body_param.entry[0].changes && 
         body_param.entry[0].changes[0].value.messages &&
@@ -42,6 +43,11 @@ app.post("/webhook", (req, res) => {
             let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+    console.log("phone number id"+phone_number_id);
+    console.log("from"+from);
+    console.log("msg_body"+msg_body);
+
 
             axios({
                 method: "POST",
