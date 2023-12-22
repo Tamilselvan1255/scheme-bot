ace8b29b2bc9a2af0bdc76e3295d07d2c5173770
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-22 12:12:39 +0530

    testing

--- index.js
+++ index.js
@@ -50,29 +50,12 @@ app.post("/webhook", (req, res) => {
                 data: {
                     messaging_product: "whatsapp",
                     to: from,
-                    data: {
-                        messaging_product: "whatsapp",
-                        to: from,
-                        template: {
-                            template_type: "button",
-                            text: "Hello, This is Tamil. Choose an option:",
-                            buttons: [
-                                {
-                                    type: "postback",
-                                    title: "Option 1",
-                                    payload: "option1"
-                                },
-                                {
-                                    type: "postback",
-                                    title: "Option 2",
-                                    payload: "option2"
-                                }
-                            ]
-                        }
+                    text: {
+                        body: "Hello, This is Tamil"
                     }
                 },
-                Headers: {
-                    "Content_Type": "application/json"
+                headers: {
+                    "Content-Type": "application/json"
                 }
             });
 
