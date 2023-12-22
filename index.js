106e73a408329a34ca61704d9825613cae419c06
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-22 13:14:41 +0530

    testing

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.post("/webhook", (req, res) => {
             // });
             axios.post(`https://graph.facebook.com/v17.0/${phone_number_id}/messages?access_token=${token}`, {
                 "messaging_product": "whatsapp",
+                "type": "individual",
                 "to": from,
                 "type": "template",
                 "template": {
