ffd0f95f04aa004c45d3842cb6c247edf8b6ccf0
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-22 19:15:26 +0530

    deploy name changed

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
-require('dotenv').config;
-const fs = require('fs');
-const FormData = require('form-data');
+require('dotenv').config();
+const app = express().use(bodyParser.json());
 
-const app = express().use(body_parser.json());
-
-const token = process.env.TOKEN; // for sending message to user
-const myToken = process.env.MYTOKEN; // for verify
+const token = process.env.TOKEN;
+const myToken = process.env.MYTOKEN;
 
 app.listen(process.env.PORT, () => {
     console.log("Webhook is listening!!");
 });
 
-// to verify the callback url from dashboard side - cloud api side
 app.get("/webhook", (req, res) => {
     let mode = req.query["hub.mode"];
     let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
 
-
     if (mode && token) {
-
         if (mode === "subscribe" && token === myToken) {
             res.status(200).send(challenge);
         } else {
@@ -31,7 +25,7 @@ app.get("/webhook", (req, res) => {
     }
 });
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
     let body_param = req.body;
     console.log(JSON.stringify(body_param, null, 2));
 
@@ -43,32 +37,48 @@ app.post("/webhook", (req, res) => {
             body_param.entry[0].changes[0].value.messages[0]
         ) {
             let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            let newTemplateMessage = "Hi there! Thanks for reaching out. Your message is important to us.";
+            let responseMessage;
+
+            // Implement your chatbot logic here
+            if (msg_body.toLowerCase().includes("hello")) {
+                responseMessage = "Hi there! How can I help you today?";
+            } else if (msg_body.toLowerCase().includes("cat")) {
+                responseMessage = "I love cats!";
+            } else if (msg_body.toLowerCase().includes("dog")) {
+                responseMessage = "I love dogs!";
+            } else {
+                responseMessage = "Sorry, I didn't understand that. Can you please clarify?";
+            }
 
             const body = {
                 "messaging_product": "whatsapp",
                 "to": "+919788825633",
                 "type": "template",
                 "template": {
-                    "name": "hello_world",
+                    "text": {
+                        "body": responseMessage
+                    },
                     "language": {
                         "code": "en_US"
                     }
                 }
             };
 
-            axios.post(`https://graph.facebook.com/v17.0/${phone_number_id}/messages?access_token=${token}`, body);
-
-            res.sendStatus(200);
+            try {
+                await axios.post(`https://graph.facebook.com/v17.0/${phone_number_id}/messages?access_token=${token}`, body);
+                res.sendStatus(200);
+            } catch (error) {
+                console.error("Error sending message:", error);
+                res.sendStatus(500);
+            }
         } else {
-            res.sendStatus(404)
+            res.sendStatus(404);
         }
     }
 });
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello, this is Webhook setup!!")
+    res.status(200).send("Webhook setup for scheme!!");
 });
