c2e838af42510c2575f47e3b60575c1fbc23cebb
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-26 00:01:25 +0530

    bot test

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.post('/whatsapp', async (req, res) => {
                 res.sendStatus(500);
                 return;
             }
-        } else if (msgBody.includes('Show Schemes')) {
+        } if (msgBody.includes('Show Schemes')) {
             // If "show schemes" is detected, respond with "deals" template
             const showSchemesTemplate = {
                 messaging_product: 'whatsapp',
