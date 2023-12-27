db7a474c2710d00b674be0f60db8fe76dbd5304b
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-27 20:46:14 +0530

    bot test

--- index.js
+++ index.js
@@ -207,65 +207,67 @@ app.post('/whatsapp', async (req, res) => {
                                 break;
 
                                 case payload === '0-6' || payload === '6-18' || payload === '18-24':
+                                    // Extract gender from the message if available
                                     const genderPayload = message.button ? message.button.gender : undefined;
-    try {
-        // Assuming schemesData is an array of scheme objects
-        const schemesData = await SchemeModel.find({ age: payload, genderEligibility: genderPayload });
-        if (schemesData.length > 0) {
-            let responseMessage = `Schemes for ${payload}:\n\n`;
-
-         schemesData.forEach((scheme) => {
-                responseMessage += 
-                    `Implemented By: ${scheme.implementedBy || 'Not available'}\n` +
-                    `Domain Description: ${scheme.domainDescription || 'Not available'}\n` +
-                    `Eligible Disabilities: ${scheme.eligibleDisabilities || 'Not available'}\n` +
-                    `Disability Percentage: ${scheme.disabilityPercentage || 'Not available'}\n` +
-                    `Age: ${scheme.age || 'Not available'}\n` +
-                    `Annual Income: ${scheme.annualIncome || 'Not available'}\n` +
-                    `Gender Eligibility: ${scheme.genderEligibility || 'Not available'}\n` +
-                    `Comments: ${scheme.comments || 'Not available'}\n` +
-                    `Email Address: ${scheme.emailAddress || 'Not available'}\n\n`;
-            });
-
-            const truncatedMessage = responseMessage.substring(0, 4096);
-
-            const responseTemplate = {
-                messaging_product: 'whatsapp',
-                to: '+919788825633',
-                type: 'text',
-                text: {
-                    body: truncatedMessage,
-                },
-                language: {
-                    code: 'en_US',
-                },
-            };
-
-            const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${token}`, responseTemplate);
-            console.log('Response:', response.data);
-            res.status(200).send(response.data);
-        } else {
-            const responseTemplate = {
-                messaging_product: 'whatsapp',
-                to: '+919788825633',
-                type: 'text',
-                text: {
-                    body: `No schemes found for the selected age group (${payload}).`,
-                },
-                language: {
-                    code: 'en_US',
-                },
-            };
-
-            const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${token}`, responseTemplate);
-            console.log('Response:', response.data);
-            res.status(200).send(response.data);
-        }
-    } catch (error) {
-        console.error('Error fetching data from the database:', error.message);
-        res.status(500).send('Internal Server Error');
-    }
-    break;
+                                
+                                    try {
+                                        // Assuming schemesData is an array of scheme objects
+                                        const schemesData = await SchemeModel.find({ age: payload, genderEligibility: genderPayload });
+                                        if (schemesData.length > 0) {
+                                            let responseMessage = `Schemes for ${payload} and ${genderPayload}:\n\n`;
+                                
+                                            schemesData.forEach((scheme) => {
+                                                responseMessage += 
+                                                    `Implemented By: ${scheme.implementedBy || 'Not available'}\n` +
+                                                    `Domain Description: ${scheme.domainDescription || 'Not available'}\n` +
+                                                    `Eligible Disabilities: ${scheme.eligibleDisabilities || 'Not available'}\n` +
+                                                    `Disability Percentage: ${scheme.disabilityPercentage || 'Not available'}\n` +
+                                                    `Age: ${scheme.age || 'Not available'}\n` +
+                                                    `Annual Income: ${scheme.annualIncome || 'Not available'}\n` +
+                                                    `Gender Eligibility: ${scheme.genderEligibility || 'Not available'}\n` +
+                                                    `Comments: ${scheme.comments || 'Not available'}\n` +
+                                                    `Email Address: ${scheme.emailAddress || 'Not available'}\n\n`;
+                                            });
+                                
+                                            const truncatedMessage = responseMessage.substring(0, 4096);
+                                
+                                            const responseTemplate = {
+                                                messaging_product: 'whatsapp',
+                                                to: '+919788825633',
+                                                type: 'text',
+                                                text: {
+                                                    body: truncatedMessage,
+                                                },
+                                                language: {
+                                                    code: 'en_US',
+                                                },
+                                            };
+                                
+                                            const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${token}`, responseTemplate);
+                                            console.log('Response:', response.data);
+                                            res.status(200).send(response.data);
+                                        } else {
+                                            const responseTemplate = {
+                                                messaging_product: 'whatsapp',
+                                                to: '+919788825633',
+                                                type: 'text',
+                                                text: {
+                                                    body: `No schemes found for the selected age group (${payload}) and gender (${genderPayload}).`,
+                                                },
+                                                language: {
+                                                    code: 'en_US',
+                                                },
+                                            };
+                                
+                                            const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${token}`, responseTemplate);
+                                            console.log('Response:', response.data);
+                                            res.status(200).send(response.data);
+                                        }
+                                    } catch (error) {
+                                        console.error('Error fetching data from the database:', error.message);
+                                        res.status(500).send('Internal Server Error');
+                                    }
+                                    break;
 
     // case payload === 'Male' || payload === 'Female' || payload === 'Both Male and Female':
     //     try {
