956505814fa78098d57c0e4334ae82d9dd793200
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-28 00:24:25 +0530

    bot test

--- index.js
+++ index.js
@@ -268,6 +268,9 @@ app.post('/whatsapp', async (req, res) => {
                                                 code: 'en_US',
                                             },
                                         };
+                                                  const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${token}`, responseTemplate);
+            console.log('Response:', response.data);
+            res.status(200).send(response.data);
                                     }
                                     break;
                                                 
